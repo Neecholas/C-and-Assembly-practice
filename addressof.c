9dafff0f34eaa34230f53a6aa355a10a9a939388
Nick Johnson <neecholas.johnson1997@gmail.com>
2018-10-05 21:32:38 +0100

    did more excircises

---
+++ addressof.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+  //creates variable 5
+  int int_var = 5;
+  //creates a pointer
+  int *int_ptr;
+
+  // sets the int_pointer pointing to
+  // the address of int_var
+  int_ptr = &int_var;
+}
